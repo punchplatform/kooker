7f9cf248bc15661de5de31dea4cfcb6409a7b8ee
Cedric VAN FRACHEM <cedric.vanfrachem@thalesgroup.com>
2022-01-21 16:37:26 +0100

    force image re-pull if it is a non-release tag (ending with something else than a digit)

--- bin/tools/docker_save.sh
+++ bin/tools/docker_save.sh
@@ -3,15 +3,29 @@
 set -o errexit
 set -o pipefail
 
+
+if [ -z ${IMG:-} ] ; then
+
+    if [ $# -ne 1 ] ; then
+        echo "FATAL: please provide image tag:version to download as first parameter or IMG environment variable" 1>&2
+        exit 1
+    fi
+    IMG=$1
+fi
+
 source bin/env.sh
 
 IMG_STRIP_FORWARD_SLASH=$(echo ${IMG} | sed 's/^\///;s/\///g')
 TARGET_ARCHIVE_PATH=${IMAGES_DIR}/${IMG_STRIP_FORWARD_SLASH}.tar.gz
 
-if [ ! -f ${TARGET_ARCHIVE_PATH} ]; then
+
+## If tag does not end with a digit, then we always pull, to allow refresh of 'floating' tag images
+
+if [[ $IMG =~ [0-9]$ ]] && [ -f ${TARGET_ARCHIVE_PATH} ] ; then
+    echo "Release image already cached: ${IMG}; skipping"
+else
+    echo "Pulling image '${IMG}"
     mkdir -p ${IMAGES_DIR}
     docker pull ${IMG}
     docker save ${IMG} > ${TARGET_ARCHIVE_PATH}
-else
-    echo "Image already cached: ${IMG}; skipping"
 fi
