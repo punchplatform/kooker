4d6081559e41dc13f82f67d723b38939aebad953
Jonathan YUE CHUN <jonathan.yue-chun@mythalesgroup.io>
2022-03-08 18:50:20 +0100

    [fix] - install kubectl

---
+++ bin/gitlab/kubectl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
